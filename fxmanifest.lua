71037103c43c400de7bc9a64baad6984f002fe90
Marcoflex23yt <marcomaino09@gmail.com>
2022-05-20 15:52:01 +0200

    Created

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'SF_ScriptPack1'
+
+version 'legacy'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+	'SrC/Main_S.lua',
+}
+
+client_scripts {
+	'SrC/Main_C.lua',
+}
